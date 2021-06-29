59f9144bf09563599b96a8438b6c8191d2b59057
Kiran <kiranb.mobio@gmail.com>
2021-06-29 11:54:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Mobio Food App</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
